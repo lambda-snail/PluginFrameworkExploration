5eeafde8aa77ce0fc3ce7ea9b1e163fdcd230254
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-08-12 16:27:13 +0200

    Added missing files

---
+++ SamplePlugins/HelloWorldPlugin.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Application.Sdk;
+
+namespace SamplePlugins;
+
+public class HelloWorldPlugin : IPlugin
+{
+    public string Name { get; set; } = "hello";
+    
+    public string Return { get; set; }
+    
+    public void Execute()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
